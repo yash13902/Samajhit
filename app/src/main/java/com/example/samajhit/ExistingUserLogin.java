2c7b748aed9a1f2f4ddc01889f4b59157a076287
yashjasani-1309 <yashbhavin.jasani2020@vitstudent.ac.in>
2021-04-09 21:25:30 +0530

    Initial commit

---
+++ app/src/main/java/com/example/samajhit/ExistingUserLogin.java
@@ -0,0 +1,14 @@
+package com.example.samajhit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ExistingUserLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_existing_user_login);
+    }
+}
